0bf49bef9635b7789a15fa1630651e6413b318ed
Gian Alexis <alex10estadistica@gmail.com>
2022-01-05 01:01:49 -0500

    listo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles.css';
 
 function App() {
+
+  return (
+    <>
+      <MyComponent />
+      <AboutMe />
+    </>
+  );
+
+}
+
+const MyComponent = () => {
+
+  const me ={
+    Nombre: "Giancarslos Crdenas Galarza",
+    Edad: "30 años",
+    Peliculas: "Loco",
+    Musica:"acdc"
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        <li> <b>Nombre:</b> {me.Nombre}</li> 
+        <li> <b>Edad:</b> {me.Edad}</li>
+        <li> <b>Pelicula favorita:</b> {me.Peliculas}</li> 
+        <li> <b>Musica favorita:</b> {me.Musica}</li> 
+      </ul>
+    </div>
+  );
+}
+
+const AboutMe = () => {
+
+  const me ={
+    Nombre: "Giancarslos Crdenas Galarza",
+    Edad: "30 años",
+    Peliculas: "Loco",
+    Musica:"acdc"
+  }
+
+  return (
+    <div className="Hob">
+      <h4 className='toto'>MIS HOBBIES</h4>
+        <ul>
+          <li>Musica</li> 
+          <li>Ejercicio</li>
+          <li>Video Juego</li> 
+          <li>Pelicula</li> 
+        </ul>
     </div>
   );
 }
 
 export default App;
+
